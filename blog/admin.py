6689e678e1e93dd8f4a746fe77d8213264316eb9
fcu-d0512825 <abcd0989290543@gmail.com>
2019-03-10 15:46:15 +0800

    Change admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Post,Tag
 # Register your models here.
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title','created_date')
+	search_fields = ('title',)
+	inlines = [TagInline]
+	fieldsets = (
+		['Main',{
+			'fields':('title','text'),
+		}],
+		['Advance',{
+			'classes': ('collapse',),
+			'fields': ('created_date',),
+		}]
+	)
+
+admin.site.register(Tag)
+admin.site.register(Post,PostAdmin)
